e97fdd35a145655af17a1922c666f257b08144f9
Constantin Scholl <constantin@scholl.im>
2016-10-02 00:09:29 -0400

    tree in python

--- pytree.py
+++ pytree.py
@@ -1,11 +1,53 @@
 #!/usr/bin/env python3
 import subprocess
 import sys
+import os
 
+# Initialize output variables
+output = ""
+count_files = 0
+count_dir = 0
 
-# YOUR CODE GOES here
+# Get root directory for tree and first line of output
+if len(sys.argv) == 1:
+    cwd = os.getcwd()
+    output += ".\n"
+else:
+    cwd = sys.argv[1]
+    output += str(cwd) + "\n"
 
+# Recursive function
+def tree(cwd, prepend=""):
+    dir_output = ""
+    dir_content = [x for x in os.listdir(cwd) if not x.startswith('.')]
+    count_files = 0
+    count_dir = 0
 
-if __name__ == '__main__':
-    # just for demo
-    subprocess.run(['tree'] + sys.argv[1:])
+    for i, line in enumerate(dir_content):
+        # If last element in directory, formating is different
+        if i == len(dir_content) - 1:
+            dir_output += prepend + "└── " + str(line) + "\n"
+            subdir_prepend = "    "
+        else:
+            dir_output += prepend + "├── " + str(line) + "\n"
+            subdir_prepend = "│   "
+
+        if os.path.isfile(os.path.join(cwd, line)):
+            count_files += 1
+        else:
+            count_dir += 1
+            result = tree(os.path.join(cwd, line), prepend + subdir_prepend)
+            dir_output += result[0]
+            count_files += result[1]
+            count_dir += result[2]
+    return dir_output, count_files, count_dir
+
+result = tree(cwd)
+output += result[0]
+count_files += result[1]
+count_dir += result[2]
+
+# Summary
+output += "\n" + str(count_dir) + " directories, " + str(count_files) + " files"
+
+print(output)
